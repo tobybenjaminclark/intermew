f213fb154b28aecaf5586b56bcae619fa63b7a28
aswarbs <amberswarbrick1@gmail.com>
2024-11-02 14:22:23 +0000

    add job descriptions

--- boogle_python/jobs_scrape.py
+++ boogle_python/jobs_scrape.py
@@ -1,4 +1,6 @@
 
+import json
+import os
 from jobspy import scrape_jobs
 import csv
 import numpy as np
@@ -11,7 +13,7 @@ class JobsScraper():
     
     def retrieve_jobs(self, search_term="software engineer"):
         jobs = scrape_jobs(
-            site_name=["indeed", "linkedin", "glassdoor", "google"],
+            site_name=["indeed", "glassdoor", "google"],
             search_term=search_term,
             google_search_term="software engineer jobs near Durham, United Kingdom since yesterday",
             location="Durham, United Kingdom",
@@ -27,7 +29,27 @@ class JobsScraper():
 
         print(jobs)
 
+        output_dir = "./job_software_engineer"
+
+        # TEMP HARDCODING FOR INTERVIEWS
+        interviews = [
+            {
+                "name": "Preliminary Round",
+                "type": "Technical"
+            },
+            {
+                "name": "Personal",
+                "type": "HR"
+            },
+            {
+                "name": "Technical Interview",
+                "type": "Managerial"
+            }
+        ]
+        
+
         for index, job in jobs.iterrows():
+
 
             title = job["title"]
             description = job["description"]
@@ -35,8 +57,25 @@ class JobsScraper():
             job_url = job["job_url"]
             is_remote = job["is_remote"]
 
-            if description == np.nan:
-                print("nan :(")
+            job_info = {
+                "position": title,
+                "name": company,
+                "description": description,
+                "url": job_url,
+                "is_remote": is_remote,
+                "interviews": interviews
+            }
+
+            # Create a safe filename using the job title
+            safe_title = job["title"].replace(" ", "_").replace("/", "_")  # Replace spaces and slashes
+            filename = f"{safe_title}_{index}.json"  # Include index to ensure uniqueness
+            file_path = os.path.join(output_dir, filename)
+
+            # Write the job info to a JSON file
+            with open(file_path, 'w') as json_file:
+                json.dump(job_info, json_file, indent=4)
+
+            print(f"Job information written to {file_path}")
 
             
 
